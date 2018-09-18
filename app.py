fc0197b27b229b8066282251db930df3ed254e98
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2018-09-18 11:50:44 +0300

    some fixes

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
-from flask import Flask, send_from_directory
-from flask_wtf.csrf import CsrfProtect
+from flask import Flask, send_from_directory, render_template
 from werkzeug.contrib.fixers import ProxyFix
 from xonfig import get_option
 import requests
@@ -24,17 +23,13 @@ app = None
 def __flask_setup():
     global app
 
-    app = Flask(__name__, static_folder='react_app/build')
+    app = Flask(__name__, static_folder='react_app/build', template_folder='react_app/build')
     app.wsgi_app = ProxyFix(app.wsgi_app)
 
     app.config['DEVELOPMENT'] = get_option('FLASK', 'DEVELOPMENT')
     app.config['DEBUG'] = get_option('FLASK', 'DEBUG')
 
     CORS(app)
-
-    if not app.config['DEVELOPMENT']:
-        csrf = CsrfProtect()
-        csrf.init_app(app)
 
 
 def __endpoint_setup():
@@ -44,7 +39,7 @@ def __endpoint_setup():
         if path != "" and os.path.exists("react_app/build/" + path):
             return send_from_directory('react_app/build', path)
         else:
-            return send_from_directory('react_app/build', 'index.html')
+            return render_template('index.html')
 
     @app.route('/api/search', methods=['POST', 'OPTIONS'])
     def search():
