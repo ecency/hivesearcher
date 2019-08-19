4ef1ef92aa4ab6a1ebccf7748477d673b804d4e5
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2019-08-19 16:32:15 +0300

    api update

--- react_app/src/components/footer/index.js
+++ react_app/src/components/footer/index.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {Link} from "react-router-dom";
 import {version} from "../../../package.json";
 
 export default class Footer extends Component {
@@ -6,8 +7,11 @@ export default class Footer extends Component {
 
         return (
             <div className="main-footer">
-                <a className="footer-brand" href="https://esteem.app/" rel="noopener noreferrer"
+                <a className="footer-link" href="https://esteem.app/" rel="noopener noreferrer"
                    target="_blank">eSteem</a>
+                <span className="separator">|</span>
+                <Link to="/api-docs" className="footer-link">API</Link>
+                <span className="space"/>
                 <span className="ver">v{version}</span>
             </div>
         )
